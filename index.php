21513996b89c982996df4d162f342b57f68a179f
MustafaTaj <for.u.400@gmail.com>
2016-06-10 17:12:18 +0300

    Update index.php

--- index.php
+++ index.php
@@ -77,7 +77,7 @@ if (! isset($SessionAction)) {
             $response = $client->sendChatAction(['chat_id' => $update->message->chat->id,
                 'action' => 'typing']);
             $response = $client->sendMessage(['chat_id' => $update->message->chat->id,
-                'text' => "الأمر المدخل غير صحيح, فضلاً إستخدم الأمر /help للحصول على قائمة الأوامر المتاحة" ]);
+                'text' => "الأمر المدخل غير صحيح, فضلاً إستخدم الأمر /help للحصول على قائمة الأوامر المتاحة : '$UniqID' - " . $_SESSION[$UniqID]]);
         }
 
     }
