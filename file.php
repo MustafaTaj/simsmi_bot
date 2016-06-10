a3d009ddfaab95e2de3a439f35c3c19563e3c296
MustafaTaj <for.u.400@gmail.com>
2016-06-10 18:25:32 +0300

    Update file.php

--- file.php
+++ file.php
@@ -1,10 +1,25 @@
 <?php
-session_start();
-echo "Hello #" . $_SESSION['count'];
-if (!isset($_SESSION['count'])) {
-    $_SESSION['count'] = 0;
+if (!defined('OIU')) {
+    define('OIU', true);
 }
-$_SESSION['count']++;
+require ($ScriptDirPath . 'includes/class.db.php');
 
+$Config['DataBase']['Host'] = 'localhost'; // سيرفر القاعدة
+$Config['DataBase']['User'] = 'be1c4294b9671e'; // مستخدم القاعدة
+$Config['DataBase']['Password'] = '56c9cfd2 '; //كلمة مرور القاعدة
+$Config['DataBase']['Name'] = 'heroku_7af4014c037034a'; // إسم القاعدة
+$url=parse_url(getenv("CLEARDB_DATABASE_URL"));
 
+  $server = $url["host"];
+  $username = $url["user"];
+  $password = $url["pass"];
+  $dbname = substr($url["path"],1);
+  
+//$db = new Database($Config['DataBase']['Host'], $Config['DataBase']['User'], $Config['DataBase']['Password'],
+//    $Config['DataBase']['Name']);
+$db = new Database($server, $username, $password,
+    $dbname);
+$db->Connection("intFunction at Line " . __line__);
+
+echo "Connected to db";
 ?>
