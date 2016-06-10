ad58d35da7a3734e252fec6c628f8c6ec7df3205
MustafaTaj <for.u.400@gmail.com>
2016-06-10 17:53:41 +0300

    Update file.php

--- file.php
+++ file.php
@@ -10,10 +10,10 @@
 * || #################################################################### ||
 * \*======================================================================*/
 ini_set('session.save_handler', 'memcached');
-ini_set('session.save_path', 'PERSISTENT=myapp_session ' . getenv('MEMCACHIER_SERVERS'));
+ini_set('session.save_path', 'mc3.dev.ec2.memcachier.com:11211');
 ini_set('memcached.sess_binary', 1);
-ini_set('memcached.sess_sasl_username', getenv('MEMCACHIER_USERNAME'));
-ini_set('memcached.sess_sasl_password', getenv('MEMCACHIER_PASSWORD'));
+ini_set('memcached.sess_sasl_username', '29667b');
+ini_set('memcached.sess_sasl_password', '412a446a87da30a3081abd036ac09202');
 
 session_start();
 echo "test1: "; print_r($_SESSION) ;
