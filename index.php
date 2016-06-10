1a5a32e2e61980b552de0d0d44d8060b0783e9ca
MustafaTaj <for.u.400@gmail.com>
2016-06-10 22:11:06 +0300

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
 require 'vendor/autoload.php';
 function CurlRequest2($fields = array("do" => "nothing"))
 {
+    global $UniqID;
     $url = "http://oiu.edu.sd/medicine/api/telegram/index.php?username=$UniqID&chatid=" .
         $update->message->chat->id . "&realusername=" . $update->message->chat->
         username;
