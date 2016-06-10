ed7bb9a796a3404ba0d946b77c2c8aebdda6cee6
MustafaTaj <for.u.400@gmail.com>
2016-06-10 20:54:13 +0300

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ $UniqID = $update->message->chat->id . "::" . $update->message->chat->username;
 //your app
 try {
     if ($update->message->text == '/me') {
-        die(json_encode(array("return_type" => "text", "return_text" => json_encode($update))));
+        json_encode(array("return_type" => "text", "return_text" => json_encode($update)));
 
     } else {
         $responses = $client->sendChatAction(['chat_id' => $update->message->chat->id,
