5ef4417c709c83806e0db5510ae2dd6348e222ba
MustafaTaj <for.u.400@gmail.com>
2016-06-10 20:35:50 +0300

    Update file.php

--- file.php
+++ file.php
@@ -1,12 +1,35 @@
 <?php
-$url=parse_url(getenv("CLEARDB_DATABASE_URL"));
+/** بسم الله الرحمن الرحيم **/
+/*/*====================================================================*\*\
+* || #################################################################### ||
+* || # Script Coded By  Mustafa Taj Elsir : www.fb.com/kemo2011         # ||
+* || # Copyright 2016  All Rights Reserved.                             # ||
+* || # This file may not be redistributed in whole or significant part. # ||
+* || # ------------------ This IS NOT A FREE SOFTWARE ----------------- # ||
+* || # -----------       Mustafa Taj Special Software      -------------- ||
+* || #################################################################### ||
+* \*======================================================================*/
 
-  $server = $url["host"];
-  $username = $url["user"];
-  $password = $url["pass"];
-  $db = substr($url["path"],1);
+        function CurlRequest($url, $fields = array("do" => "nothing"))
+    {
+        $fields_string = '';
+        foreach ($fields as $key => $value) {
+            $fields_string .= $key . '=' . urlencode($value) . '&';
+        }
+        rtrim($fields_string, '&');
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, count($fields));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        $result = preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $result);
+        return json_decode($result, true);
+    }
+        $fields = array("replay" => "/help");
+        $response = CurlRequest("http://oiu.edu.sd/medicine/api/telegram/index.php?username=$UniqID",
+            $fields);
+        print_r($response);
 
-  mysql_connect($server, $username, $password);
-
-  mysql_select_db($db);
 ?>
