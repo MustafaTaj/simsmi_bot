fee8757abe5dbbdb19f259d5be8133589f248170
MustafaTaj <for.u.400@gmail.com>
2016-06-10 17:35:17 +0300

    Update file.php

--- file.php
+++ file.php
@@ -14,20 +14,7 @@ session_start();
 session_decode(fputs($sessionfile,  4096) );
 fclose($sessionfile);*/
 
-echo "Session - " . $_SESSION["testing"];
-$_SESSION["testing"] = "tt" ;
-
-echo "<br />" . session_encode( ) ;
-$sessionfile = fopen("sessionfile.txt", "w");
-fputs($sessionfile, session_encode( ) );
-fclose($sessionfile);
-?>=======================================*/
-session_start();
-$sessionfile = fopen("sessionfile.txt", "r");
-session_decode(fputs($sessionfile,  4096) );
-fclose($sessionfile);
-
-echo "Session - " . $_SESSION["testing"];
+echo "Session | " . $_SESSION["testing"];
 $_SESSION["testing"] = "tt" ;
 
 echo "<br />" . session_encode( ) ;
