a587946a39cd3e18ebbb58c3a186accd1ba39155
MustafaTaj <for.u.400@gmail.com>
2016-06-10 21:19:29 +0300

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,23 @@
 * \*======================================================================*/
 
 require 'vendor/autoload.php';
-
+function CurlRequest2($url, $fields = array("do" => "nothing"))
+    {
+        $fields_string = '';
+        foreach ($fields as $key => $value) {
+            $fields_string .= $key . '=' . urlencode($value) . '&';
+        }
+        rtrim($fields_string, '&');
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, count($fields));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        $result = preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $result);
+        return json_decode($result, true);
+    }
 $client = new Zelenin\Telegram\Bot\Api('183692296:AAEsT63R1yvvYMsWCm0t9NEhUz-OYEByA3c'); // Set your access token
 $url = 'https://oiu-medicine.herokuapp.com/'; // URL RSS feed
 $update = json_decode(file_get_contents('php://input'));
@@ -30,7 +46,7 @@ try {
         $responses = $client->sendChatAction(['chat_id' => $update->message->chat->id,
             'action' => 'typing']);
         $fields = array("replay" => $update->message->text);
-        $response = CurlRequest("http://oiu.edu.sd/medicine/api/telegram/index.php?username=$UniqID",
+        $response = CurlRequest2("http://oiu.edu.sd/medicine/api/telegram/index.php?username=$UniqID",
             $fields);
         if ($response["return_type"] == "text")
             $response = $client->sendMessage(['chat_id' => $update->message->chat->id,
