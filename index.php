d3e643784d822e035071c94a8b12039d55217faa
MustafaTaj <for.u.400@gmail.com>
2016-06-22 16:35:58 +0300

    Update index.php

--- index.php
+++ index.php
@@ -66,16 +66,16 @@ try {
                 'caption' => $response["return_text"], 'document' => $response['return_filecontent']]);
         } elseif ($response["return_type"] == "mutlifile") {
             $FilesList = json_decode($response['files_list']);
-            $client->sendMessage(['chat_id' => $update->message->chat->id,
-                'text' => "Testing: \n\n\n" . json_encode($response)]);
-            /*foreach ($FilesList as $file) {
-            if ($file["type"] == 'document')
+            //$client->sendMessage(['chat_id' => $update->message->chat->id,
+            //    'text' => "Testing: \n\n\n" . json_encode($response)]);
+            foreach ($FilesList as $file) {
+            if ($file["type"] == 'document' || $file["type"] == '')
             $response = $client->sendDocument(['chat_id' => $update->message->chat->id,
             'caption' => $file["return_text"], 'document' => $file['fileid']]);
             elseif ($file["type"] == 'photo')
             $response = $client->sendDocument(['chat_id' => $update->message->chat->id,
             'caption' => $file["return_text"], 'photo' => $file['fileid']]);
-            }*/
+            }
         }
     }
 }
