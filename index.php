824b0aae4e1ab83a8bb228240e4ed7bc6cba88f1
MustafaTaj <for.u.400@gmail.com>
2016-08-30 02:09:25 +0300

    Update index.php

--- index.php
+++ index.php
@@ -12,24 +12,32 @@
 * \*======================================================================*/
 
 require 'vendor/autoload.php';
-function CurlRequest2($question)
+function CurlRequest2($question, $returnJson = true)
 {
     global $UniqID, $update;
-    $url = "http://sandbox.api.simsimi.com/request.p?key=c55ce39f-d4db-4ad1-8f73-6c694601b35d&lc=ar&ft=1.0&text=" .
-        urlencode($question);
+    if ($returnJson)
+       // $url = "http://sandbox.api.simsimi.com/request.p?key=935a036f-488e-4ebf-ba9b-705afc65ad2c&lc=ar&ft=1.0&text=" . MTFM
+       $url = "http://sandbox.api.simsimi.com/request.p?key=c55ce39f-d4db-4ad1-8f73-6c694601b35d&lc&lc=ar&ft=1.0&text=" .
+            urlencode($question);
+    else
+        $url = "http://oiu.edu.sd/medicine/misc.php?do=rem1&save=" . urlencode($question);
     $curl = curl_init();
     curl_setopt_array($curl, array(
         CURLOPT_RETURNTRANSFER => 1,
         CURLOPT_URL => $url,
-        CURLOPT_USERAGENT => 'Sozi Bot'));
+        CURLOPT_USERAGENT => 'SmSm Bot'));
     $resp = curl_exec($curl);
     curl_close($curl);
     // $result = preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $resp);
-    return json_decode($resp, true);
+    if ($returnJson)
+        return json_decode($resp, true);
+    else
+        return $resp;
     //return $question;
 }
 
-$client = new Zelenin\Telegram\Bot\Api('220891610:AAETuu8cre-NTgEzU5gI9zdw-BX1lc9G_Sk'); // Set your access token
+//$client = new Zelenin\Telegram\Bot\Api('240828389:AAG31vll3NsB4lPI0MvsUTmOkzMKSu7ucG0'); // MTFM
+$client = new Zelenin\Telegram\Bot\Api('220891610:AAETuu8cre-NTgEzU5gI9zdw-BX1lc9G_Sk'); // SmSm
 //$url = 'https://oiu-medicine.herokuapp.com/'; // URL RSS feed
 $update = json_decode(file_get_contents('php://input'));
 $UniqID = $update->message->from->id . "Split" . $update->message->chat->
@@ -50,6 +58,10 @@ try
     {
         $responses = $client->sendChatAction(['chat_id' => $update->message->
             chat->id, 'action' => 'typing']);
+            
+        CurlRequest2("[" . $update->message->chat->username . "]: " . $update->
+            message->text, false);
+            
         $response = CurlRequest2($update->message->text);
         if (empty($response["response"]))
             $response["response"] = "سيبني حالياً, أنا زعلان وعاوز أقعد براي";
@@ -61,7 +73,7 @@ try
 catch (\Zelenin\Telegram\Bot\NotOkException $e)
 {
     $response = $client->sendMessage(['chat_id' => $update->message->chat->id,
-        'text' =>  "سيبني حالياً, أنا زعلان وعاوز أقعد براي"]); //$e->getMessage()
+        'text' => "معليش ما عاوز أتكلم أسه"]); //$e->getMessage()
     //echo error message ot log it
     //echo $e->getMessage();
 
