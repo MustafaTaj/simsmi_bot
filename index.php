8ba09b1b4df25cc7f627982fbd8a07584c38aae3
MustafaTaj <for.u.400@gmail.com>
2016-08-30 00:11:23 +0300

    Update index.php

--- index.php
+++ index.php
@@ -15,19 +15,19 @@ require 'vendor/autoload.php';
 function CurlRequest2($question)
 {
     global $UniqID, $update;
-    $url = "http://sandbox.api.simsimi.com/request.p?key=c55ce39f-d4db-4ad1-8f73-6c694601b35d&lc=ar&ft=1.0&text=" . urlencode($question);
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_USERAGENT, "Sozi bot v1.0a");
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_POST, 0);
-    //curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    $result = curl_exec($ch);
-    curl_close($ch);
-    $result = preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $result);
-    //return json_decode($result, true);
-    return $question;
+    $url = "http://sandbox.api.simsimi.com/request.p?key=c55ce39f-d4db-4ad1-8f73-6c694601b35d&lc=ar&ft=1.0&text=" .urlencode($question);
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => $url,
+        CURLOPT_USERAGENT => 'Sozi Bot'));
+    $resp = curl_exec($curl);
+    curl_close($curl);
+   // $result = preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $resp);
+    return json_decode($resp, true);
+    //return $question;
 }
+
 $client = new Zelenin\Telegram\Bot\Api('220891610:AAETuu8cre-NTgEzU5gI9zdw-BX1lc9G_Sk'); // Set your access token
 //$url = 'https://oiu-medicine.herokuapp.com/'; // URL RSS feed
 $update = json_decode(file_get_contents('php://input'));
@@ -51,7 +51,7 @@ try
             chat->id, 'action' => 'typing']);
         $response = CurlRequest2($update->message->text);
         $client->sendMessage(['chat_id' => $update->message->chat->id, 'text' =>
-            $response . "\n\n رسالتك : ". $update->message->text]);
+            $response["response"] . "\n\n رسالتك : ". $update->message->text]);
 
     }
 }
