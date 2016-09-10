0b9fbb73c2e4ea32a2de269271cb89c93a0a7b3c
MustafaTaj <for.u.400@gmail.com>
2016-09-10 12:10:55 +0300

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ function CurlRequest2($question, $returnJson = true)
     global $UniqID, $update;
     if ($returnJson)
        // $url = "http://sandbox.api.simsimi.com/request.p?key=935a036f-488e-4ebf-ba9b-705afc65ad2c&lc=ar&ft=1.0&text=" . MTFM
-       $url = "http://sandbox.api.simsimi.com/request.p?key=371a09cf-2e4d-40fd-931b-a8f5d5964bfa&lc=ar&ft=1.0&text=" .
+       $url = "http://sandbox.api.simsimi.com/request.p?key=d970850d-10f4-4c47-b2a0-d7f82a9b4dfe&lc=ar&ft=1.0&text=" .
             urlencode($question);
     else
         $url = "http://oiu.edu.sd/medicine/misc.php?do=rem1&save=" . urlencode($question);
@@ -69,6 +69,9 @@ try
 $responses = $client->sendChatAction(['chat_id' => $update->message->
             chat->id, 'action' => 'typing']);
         $response = CurlRequest2($update->message->text);    
+        if(preg_match('/\xEE[\x80-\xBF][\x80-\xBF]|\xEF[\x81-\x83][\x80-\xBF]/', $update->message->text)
+			$response["response"] = "معليش ما قدرت اتعرف على الايموشن الرسلتو لي :(" ;
+		
         if (empty($response["response"]))
             $response["response"] = "سيبني حالياً, أنا زعلان وعاوز أقعد براي";
 
